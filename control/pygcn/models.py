1b1ac382234833f1f74eff14d89839b75915aa31
msgerasyov <msgerasyov@edu.hse.ru>
2021-05-04 21:14:56 +0300

    Update models

--- control/pygcn/models.py
+++ control/pygcn/models.py
@@ -1,18 +1,25 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from pygcn.layers import GraphConvolution
+import dgl
+from dgl.nn import GATConv, SAGEConv, GraphConv
+import networkx as nx
 
 
 class GCN(nn.Module):
     def __init__(self, nfeat, nhid, nclass=2, dropout=False):
         super(GCN, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nhid)
-        self.gc2 = GraphConvolution(nhid, nclass)
+        self.gc1= GraphConv(nfeat, nhid)
+        self.gc2 = GraphConv(nhid, nclass)
         self.dropout = dropout
 
     def forward(self, x, adj):
-        x = F.relu(self.gc1(x, adj))
+        if adj.is_sparse:
+            adj = adj.to_dense()
+        G = nx.from_numpy_matrix(adj.data.cpu().numpy())
+        g = dgl.from_networkx(G)
+        x = F.relu(self.gc1(g, x))
         x = F.dropout(x, self.dropout, training=self.training)
-        x = self.gc2(x, adj)
+        x = self.gc2(g, x)
         return F.log_softmax(x, dim=1)
